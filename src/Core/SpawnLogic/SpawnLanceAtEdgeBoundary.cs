ab056cd424f0b476c28dc5acc9651db53364346f
Richard Griffiths <cwolfs@gmail.com>
2018-09-12 15:12:45 +0200

    Initial rec code

--- src/Core/SpawnLogic/SpawnLanceAtEdgeBoundary.cs
+++ src/Core/SpawnLogic/SpawnLanceAtEdgeBoundary.cs
@@ -9,6 +9,11 @@ namespace SpawnVariation.Logic {
   public class SpawnLanceAtEdgeOfBoundary : SpawnLogic {
     public SpawnLanceAtEdgeOfBoundary(GameObject lanceGameObject) : base() {
       Main.Logger.Log($"[SpawnLanceAtEdgeOfBoundary] For {lanceGameObject.name}");
+      SpawnManager spawnManager = SpawnManager.GetInstance();
+      GameObject chunkBoundaryRect = spawnManager.EncounterLayerGameObject.transform.Find("Chunk_EncounterBoundary").gameObject;
+      GameObject boundary = chunkBoundaryRect.transform.Find("EncounterBoundaryRect").gameObject;
+      EncounterBoundaryRectGameLogic boundaryLogic = boundary.GetComponent<EncounterBoundaryRectGameLogic>();
+      Rect boundaryRec = boundaryLogic.GetRect();
     }
   }
 }
