18af90e1f26c83d96cbf3662cdef179ae27f8dd1
Richard Griffiths <cwolfs@gmail.com>
2018-09-21 09:44:35 +0200

    Reduced code for the same system

--- src/Patches/EncounterLayerParentInitializeContractPatch.cs
+++ src/Patches/EncounterLayerParentInitializeContractPatch.cs
@@ -13,22 +13,8 @@ namespace SpawnVariation.Patches {
   public class EncounterLayerParentInitializeContractPatch {
     static void Prefix(EncounterLayerParent __instance) {
       Main.Logger.Log($"[EncounterLayerParentInitializeContractPatch Prefix] Patching InitializeContract");
-      SetupEncounterPreReqLogic(__instance);
-
       EncounterManager encounterManager = EncounterManager.GetInstance();
       encounterManager.CreateDestroyWholeLanceObjective();
-    }
-
-    static void SetupEncounterPreReqLogic(EncounterLayerParent encounterLayerParent) {
-      Contract activeContract = UnityGameInstance.BattleTechGame.Combat.ActiveContract;
-      string encounterObjectGuid = activeContract.encounterObjectGuid;
-
-      for (int i = 0; i < encounterLayerParent.EncounterLayerList.Length; i++) {
-        encounterLayerParent.EncounterLayerList[i].gameObject.SetActive(false);
-      }
-
-      EncounterLayerData selectedEncounterLayerData = encounterLayerParent.GetLayerByGuid(encounterObjectGuid);
-      selectedEncounterLayerData.gameObject.SetActive(true);
     }
   }
 }
