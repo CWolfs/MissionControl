0c43bf2d5d0d9ff5ba5d3a6e9a1d921023b00c92
Richard Griffiths <cwolfs@gmail.com>
2018-09-28 15:30:49 +0200

    Laptop

--- src/Core/Settings/Lance.cs
+++ src/Core/Settings/Lance.cs
@@ -2,14 +2,11 @@ using Newtonsoft.Json;
 
 namespace MissionControl.Config {
 	public class Lance {
-		[JsonProperty("Min")]
-		public int Min { get; set; } = 0;
-
 		[JsonProperty("Max")]
 		public int Max { get; set; } = 0;
 
 		public int SelectNumberOfAdditionalLances() {
-			int lanceNumber = UnityEngine.Random.Range(Min, Max);
+			int lanceNumber = UnityEngine.Random.Range(1, Max);
 			Main.Logger.Log($"[SelectNumberOfAdditionalLances] {lanceNumber}");
 			return lanceNumber;
 		}
