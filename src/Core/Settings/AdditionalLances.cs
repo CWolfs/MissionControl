632f5a5c341c038dc2a564c443094c8ba0223e8d
Richard Griffiths <cwolfs@gmail.com>
2018-09-30 21:25:39 +0200

    Load additional config options

--- src/Core/Settings/AdditionalLances.cs
+++ src/Core/Settings/AdditionalLances.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 using Newtonsoft.Json;
 
 namespace MissionControl.Config {
     public class AdditionalLances {
+        [JsonProperty("IncludeContractTypes")]
+        public List<string> IncludeContractTypes { get; set; } = new List<string>(){ "ALL" };
+
+        [JsonProperty("ExcludeContractTypes")]
+        public List<string> ExcludeContractTypes { get; set; } = new List<string>();
+
+        [JsonProperty("LancePool")]
+        public Dictionary<string, List<string>> LancePool { get; set; } = new Dictionary<string, List<string>>();
+
         [JsonProperty("Player")]
         public Lance Player { get; set; } = new Lance();
 
--- src/Core/Settings/Lance.cs
+++ src/Core/Settings/Lance.cs
@@ -1,9 +1,20 @@
+using System.Collections.Generic;
+
 using Newtonsoft.Json;
 
 namespace MissionControl.Config {
 	public class Lance {
 		[JsonProperty("Max")]
 		public int Max { get; set; } = 0;
+
+		[JsonProperty("ExcludeContractTypes")]
+		public List<string> ExcludeContractTypes { get; set; } = new List<string>();
+
+		[JsonProperty("ChanceToSpawn")]
+		public float ChanceToSpawn = 0;
+
+		[JsonProperty("LancePool")]
+		public Dictionary<string, List<string>> LancePool { get; set; } = new Dictionary<string, List<string>>();
 
 		public int SelectNumberOfAdditionalLances() {
 			int lanceNumber = UnityEngine.Random.Range(1, Max);
