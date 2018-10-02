6db0ee03ca75a5432f25c1262676acb2344c60b5
Richard Griffiths <cwolfs@gmail.com>
2018-10-02 18:50:38 +0200

    Added a test log dump of top level behaviour nodes for info gathering

--- src/Core/AiManager.cs
+++ src/Core/AiManager.cs
@@ -1,3 +1,10 @@
+using System.Linq;
+using System.Reflection;
+
+using Harmony;
+
+using MissionControl.Utils;
+
 namespace MissionControl {
   public class AiManager {
     private static AiManager instance;
@@ -10,8 +17,17 @@ namespace MissionControl {
 
     private AiManager() {}
 
-    public void AddCustomRootLeafBehaviourSequences(BehaviorTree behaviourTree) {
-      
+    public void AddCustomRootLeafBehaviourSequences(BehaviorTree behaviourTree, BehaviorTreeIDEnum behaviourTreeType) {
+      BehaviorNode rootNode = behaviourTree.RootNode;
+
+      if (behaviourTreeType == BehaviorTreeIDEnum.CoreAITree) {
+        SelectorNode coreAiRoot = (SelectorNode)rootNode;
+        coreAiRoot.Children.FindIndex(child => {
+          string name = (string)AccessTools.Field(typeof(BehaviorNode), "name").GetValue(child);
+          Main.Logger.Log($"[AddCustomRootLeafBehaviourSequences] BehaviourNode name is '{name}'");
+          return false;
+        });
+      }
     }
   }
 }
