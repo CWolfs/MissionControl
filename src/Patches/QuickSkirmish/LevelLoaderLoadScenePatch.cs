b258c07aeec287e17811f5d924a98ceae3d56b02
Richard Griffiths <cwolfs@gmail.com>
2018-10-19 11:18:13 +0200

    Added a check for debug skirmish for the level loader

--- src/Patches/QuickSkirmish/LevelLoaderLoadScenePatch.cs
+++ src/Patches/QuickSkirmish/LevelLoaderLoadScenePatch.cs
@@ -14,7 +14,7 @@ namespace MissionControl.Patches {
   [HarmonyPatch(typeof(LevelLoader), "LoadScene")]
   public class LevelLoaderLoadScenePatch {
     static void Prefix(LevelLoader __instance, string scene) {
-      if (scene == "MainMenu") {
+      if (Main.Settings.DebugSkirmishMode && scene == "MainMenu") {
         UiManager.Instance.ShouldPatchMainMenu = true;
       }
     }
