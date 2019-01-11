a5506a85cf40dc779c0dc8c8732532ced0666e12
Richard Griffiths <cwolfs@gmail.com>
2019-01-11 21:42:20 +0100

    Added a higher chance to use faction last names for runtime cast

--- src/Core/DataManager.cs
+++ src/Core/DataManager.cs
@@ -175,9 +175,16 @@ namespace MissionControl {
     }
 
     public string GetRandomLastName(string factionKey) {
-      List<string> names = new List<string>();
-      names.AddRange(this.LastNames["All"]);
-      if (this.LastNames.ContainsKey(factionKey)) names.AddRange(this.LastNames[factionKey]);
+      List<string> names = null;
+      float chance = UnityEngine.Random.Range(0f, 100f);
+      bool useFactionName = false;
+      if (chance < 75) useFactionName = true;
+
+      if (this.LastNames.ContainsKey(factionKey) && this.LastNames[factionKey].Count > 0 && useFactionName) {
+        names = this.LastNames[factionKey];
+      } else {
+        names = this.LastNames["All"];
+      }
 
       return names[UnityEngine.Random.Range(0, names.Count)];
     }
