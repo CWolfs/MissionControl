21ada927063d26966d91321a0f06a55772d6a5ae
Richard Griffiths <cwolfs@gmail.com>
2019-07-22 23:58:09 +0200

    Added rules for FireMission and AttackDefend

---
+++ src/Core/EncounterRules/FireMission/FireMissionEncounterRules.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using BattleTech;
+
+using MissionControl.Trigger;
+using MissionControl.Logic;
+
+namespace MissionControl.Rules {
+  public class FireMissionEncounterRules : EncounterRules {
+    public FireMissionEncounterRules() : base() { }
+
+    public override void Build() {
+      Main.Logger.Log("[FireMissionEncounterRules] Setting up rule object references");
+      BuildAi();
+      BuildSpawns();
+      BuildAdditionalLances("LanceEnemyOpposingForce", SpawnLogic.LookDirection.AWAY_FROM_TARGET,
+        "SpawnerPlayerLance", SpawnLogic.LookDirection.AWAY_FROM_TARGET, 25f, 100f);
+    }
+
+    public void BuildAi() {
+      EncounterLogic.Add(new IssueFollowLanceOrderTrigger(new List<string>(){ Tags.EMPLOYER_TEAM }, IssueAIOrderTo.ToLance, new List<string>() { Tags.PLAYER_1_TEAM }));
+    }
+
+    public void BuildSpawns() {
+      Main.Logger.Log("[FireMissionEncounterRules] Building spawns rules");
+      EncounterLogic.Add(new SpawnLanceAtEdgeOfBoundary(this, "SpawnerPlayerLance", "LanceEnemyOpposingForce", 400f));
+    }
+
+    public override void LinkObjectReferences(string mapName) {
+      ObjectLookup.Add("LanceEnemyOpposingForce", EncounterLayerData.gameObject.FindRecursive("Lance_Enemy_OpposingForce"));
+    }
+  }
+}
