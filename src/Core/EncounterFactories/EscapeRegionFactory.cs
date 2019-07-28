9bacec4e6e6898f91c4484507ca5a722e6cfb52e
Richard Griffiths <cwolfs@gmail.com>
2019-07-28 14:52:53 +0200

    Add mesh generation

--- src/Core/EncounterFactories/EscapeRegionFactory.cs
+++ src/Core/EncounterFactories/EscapeRegionFactory.cs
@@ -5,6 +5,8 @@ using BattleTech;
 using BattleTech.Designed;
 using BattleTech.Framework;
 
+using MissionControl.Utils;
+
 namespace MissionControl.EncounterFactories {
   public class EscapeRegionFactory {
     private static float REGION_RADIUS = 70.71068f;
@@ -32,24 +34,35 @@ namespace MissionControl.EncounterFactories {
 
       // Test position
       escapeRegionGo.transform.position = new Vector3(-90, 300, 0);
+      // End Test
 
-      escapeRegionGo.AddComponent<MeshCollider>();
-      escapeRegionGo.AddComponent<MeshFilter>();
+      MeshCollider collider = escapeRegionGo.AddComponent<MeshCollider>();
+      MeshFilter mf = escapeRegionGo.AddComponent<MeshFilter>();
+
+      // TODO: Feed the height into this so it matches the terrain. Same as the below region points vectors
+      Mesh mesh = MeshTools.CreateHexigon(REGION_RADIUS);
+      collider.sharedMesh = mesh;
+      mf.mesh = mesh;
+
       escapeRegionGo.AddComponent<TerrainDataChangeDetection>();
       escapeRegionGo.AddComponent<SnapToTerrain>();
+      escapeRegionGo.AddComponent<MeshRenderer>();
 
       RegionGameLogic regionGameLogic = escapeRegionGo.AddComponent<RegionGameLogic>();
       regionGameLogic.encounterObjectGuid = Guid.NewGuid().ToString();
       regionGameLogic.radius = REGION_RADIUS;
       regionGameLogic.regionDefId = "regionDef_EvacZone";
+      regionGameLogic.alwaysShowRegionWhenActive = true;
+
+
       // TODO: Add objective references
 
       CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint1", new Vector3(0, 0, REGION_RADIUS));                   // North
-      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint2", new Vector3(REGION_RADIUS, 0, REGION_RADIUS / 2));   // North-East
-      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint3", new Vector3(REGION_RADIUS, 0, -REGION_RADIUS / 2));  // South-East
+      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint2", new Vector3(REGION_RADIUS, 0, REGION_RADIUS / 2f));   // North-East
+      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint3", new Vector3(REGION_RADIUS, 0, -(REGION_RADIUS / 2f)));  // South-East
       CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint4", new Vector3(0, 0, -REGION_RADIUS));                  // South
-      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint5", new Vector3(-REGION_RADIUS, 0, -REGION_RADIUS / 2)); // South-West
-      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint6", new Vector3(-REGION_RADIUS, 0, REGION_RADIUS / 2));  // North-West
+      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint5", new Vector3(-REGION_RADIUS, 0, -(REGION_RADIUS / 2f))); // South-West
+      CreateRegionPointGameObject(escapeRegionGo, $"RegionPoint6", new Vector3(-REGION_RADIUS, 0, REGION_RADIUS / 2f));  // North-West
 
       return regionGameLogic;
     }
---
+++ src/Util/MeshTools.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace MissionControl.Utils {
+  public class MeshTools {
+    public static Mesh CreateHexigon(float radius) {
+      Vector2[] vertices2d = new Vector2[] {
+        new Vector2(0, radius),
+        new Vector2(radius, radius / 2f),
+        new Vector2(radius, -(radius / 2f)),
+        new Vector2(0, -radius),
+        new Vector2(-radius, -(radius / 2f)),
+        new Vector2(-radius, radius / 2f),
+      };
+
+      Triangulator tr = new Triangulator(vertices2d);
+      int[] indices = tr.Triangulate();
+
+      Vector3[] vertices = new Vector3[vertices2d.Length];
+      for (int i = 0; i < vertices.Length; i++) {
+          vertices[i] = new Vector3(vertices2d[i].x, 0, vertices2d[i].y);
+      }
+
+      Mesh mesh = new Mesh();
+      mesh.vertices = vertices;
+      mesh.triangles = indices;
+      mesh.RecalculateNormals();
+      mesh.RecalculateBounds();
+
+      return mesh;
+    }
+  }
+}
