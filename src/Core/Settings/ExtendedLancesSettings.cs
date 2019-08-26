78a190b6ab5f29d269de2f3840f8a4101a1bba51
Richard Griffiths <cwolfs@gmail.com>
2019-08-26 13:52:54 +0200

    Support new settings json format for Extended Lances

--- src/Core/Settings/ExtendedLancesSettings.cs
+++ src/Core/Settings/ExtendedLancesSettings.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 
 using Newtonsoft.Json;
@@ -11,17 +12,34 @@ namespace MissionControl.Config {
 		public bool Autofill { get; set; } = true;
 
 		[JsonProperty("LanceSizes")]
-		public Dictionary<string, List<string>> LanceSizes { get; set; } = new Dictionary<string, List<string>>();
+		public Dictionary<string, List<ExtendedLance>> LanceSizes { get; set; } = new Dictionary<string, List<ExtendedLance>>();
 
 		public int GetFactionLanceSize(string factionKey) {
-			foreach (KeyValuePair<string, List<string>> lanceSetPair in LanceSizes) {
+			foreach (KeyValuePair<string, List<ExtendedLance>> lanceSetPair in LanceSizes) {
 				int lanceSize = int.Parse(lanceSetPair.Key);
-				List<string> factions = lanceSetPair.Value;
+				List<ExtendedLance> factions = lanceSetPair.Value;
 
-				if (factions.Contains(factionKey)) return lanceSize;
+				ExtendedLance lance = factions.FirstOrDefault(extendedLance => extendedLance.Faction == factionKey);
+
+				if (lance != null) return lanceSize;
 			}
 
 			return 4;
 		}
+
+		// public int GetFactionLanceDifficulty(string factionKey) {
+			/*
+			foreach (KeyValuePair<string, List<ExtendedLance>> lanceSetPair in LanceSizes) {
+				int lanceSize = int.Parse(lanceSetPair.Key);
+				List<ExtendedLance> factions = lanceSetPair.Value;
+
+				ExtendedLance lance = factions.FirstOrDefault(extendedLance => extendedLance.Faction == factionKey);
+
+				if (lance != null) return lanceSize;
+			}
+
+			return 4;
+			*/
+		// }
 	}
 }
