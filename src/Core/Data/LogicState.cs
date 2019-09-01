bcdcb7245716b87911248a6e2d200d53d0faca04
Richard Griffiths <cwolfs@gmail.com>
2019-09-01 22:12:51 +0200

    Possible fix for bad extended lance spawns

--- src/Core/Data/LogicState.cs
+++ src/Core/Data/LogicState.cs
@@ -2,15 +2,29 @@ using System.Collections.Generic;
 
 namespace MissionControl.Logic {
   public class LogicState {
-    public Dictionary<string, bool> state = new Dictionary<string, bool>();
+    public Dictionary<string, bool> boolState = new Dictionary<string, bool>();
+    public Dictionary<string, object> objectState = new Dictionary<string, object>();
 
     public void Set(string key, bool flag) {
-      state[key] = flag;
+      boolState[key] = flag;
+    }
+
+    public void Set(string key, List<string> keys) {
+      objectState.Add(key, keys);
+    }
+
+    public void Set(string key, List<string[]> keys) {
+      objectState.Add(key, keys);
     }
 
     public bool GetBool(string key) {
-      if (state.ContainsKey(key)) return state[key];
+      if (boolState.ContainsKey(key)) return boolState[key];
       return false;
+    }
+
+    public object GetObject(string key) {
+      if (objectState.ContainsKey(key)) return objectState[key];
+      return null;
     }
   }
 }
