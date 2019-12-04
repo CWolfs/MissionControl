3e72fab0092eb513ef7b51a88957624c1c5fec96
Richard Griffiths <cwolfs@gmail.com>
2019-12-04 18:52:58 +0100

    Added more components to the encounter layer go

--- src/Core/EncounterFactories/EncounterLayerFactory.cs
+++ src/Core/EncounterFactories/EncounterLayerFactory.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 using BattleTech;
 using BattleTech.Data;
+using BattleTech.Framework;
 
 namespace MissionControl.EncounterFactories {
   public class EncounterLayerFactory {
@@ -11,6 +12,13 @@ namespace MissionControl.EncounterFactories {
       GameObject encounterLayerGo = new GameObject(encounterLayerMDD.Name);
       EncounterLayerData encounterLayer = encounterLayerGo.AddComponent<EncounterLayerData>();
       encounterLayer.encounterObjectGuid = contract.encounterObjectGuid;
+
+      encounterLayerGo.AddComponent<PlotOverride>();
+      encounterLayerGo.AddComponent<ContractObjectiveGameLogic>();
+
+      ContractAdapter contractAdapter = encounterLayerGo.AddComponent<ContractAdapter>();
+      contractAdapter.contractOverride = contract.Override;
+      contractAdapter.ApplyContractToLayer(encounterLayer, false);
 
       return encounterLayer;
     }
