afb3625112521117d6a258a3b84fb100a846afba
Richard Griffiths <cwolfs@gmail.com>
2019-12-08 22:57:15 +0100

    Copy assets with one task

---
+++ copy-assets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf 'D:/Program Files (x86)/Steam/steamapps/common/BATTLETECH/Mods/MissionControl/contractTypeBuilds'
+rm -rf 'D:/Program Files (x86)/Steam/steamapps/common/BATTLETECH/Mods/MissionControl/overrides'
+
+cp -r 'contractTypeBuilds/' 'D:/Program Files (x86)/Steam/steamapps/common/BATTLETECH/Mods/MissionControl'
+cp -r 'overrides/' 'D:/Program Files (x86)/Steam/steamapps/common/BATTLETECH/Mods/MissionControl'
+cp -r 'mod.json' 'D:/Program Files (x86)/Steam/steamapps/common/BATTLETECH/Mods/MissionControl'
