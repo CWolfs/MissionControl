3e50a10176846c8d78f49f50a0a1c1ca8ffebbd8
Richard Griffiths <cwolfs@gmail.com>
2019-12-17 10:29:41 +0100

    Hook up swap placement logic

--- src/Core/LogicComponents/Placers/SwapPlacementGameLogic.cs
+++ src/Core/LogicComponents/Placers/SwapPlacementGameLogic.cs
@@ -20,6 +20,20 @@ namespace MissionControl.LogicComponents.Placers {
       }
     }
 
+    public override void AlwaysInit(CombatGameState combat) {
+      base.AlwaysInit(combat);
+      this.messageMemory.TrackMethod(MessageCenterMessageType.OnInitializeContractComplete, new ReceiveMessageCenterMessage(this.OnInitializeContractComplete));
+    }
+
+    protected override void SubscribeToMessages(bool shouldAdd) {
+      this.messageMemory.Subscribe(MessageCenterMessageType.OnInitializeContractComplete, new ReceiveMessageCenterMessage(this.OnInitializeContractComplete), shouldAdd);
+      base.SubscribeToMessages(shouldAdd);
+    }
+
+    private void OnInitializeContractComplete(MessageCenterMessage message) {
+      Main.LogDebug($"[SwapPlacementGameLogic.OnInitializeContractComplete] {message.ToString()}");
+    }
+
     public override void FromJSON(string json) {
       JSONSerializationUtility.FromJSON<SwapPlacementGameLogic>(this, json);
     }
