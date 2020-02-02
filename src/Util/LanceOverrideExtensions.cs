f3102d373c30515ec4ecd9f536ab8c9260864116
Richard Griffiths <cwolfs@gmail.com>
2020-02-03 00:43:06 +0100

    Include inherit lance

--- src/Util/LanceOverrideExtensions.cs
+++ src/Util/LanceOverrideExtensions.cs
@@ -8,12 +8,12 @@ using MissionControl;
 using BattleTech.Framework;
 
 public static class LanceOverrideExtensions {
-  public static UnitSpawnPointOverride GetAnyTaggedLanceMember(this LanceOverride lanceOverride) {  
-      List<UnitSpawnPointOverride> unitSpawnOverrides = lanceOverride.unitSpawnPointOverrideList;
-      foreach (UnitSpawnPointOverride unitSpawnOverride in unitSpawnOverrides) {
-        if ((unitSpawnOverride.unitDefId == "Tagged") || (unitSpawnOverride.unitDefId == "UseLance")) return unitSpawnOverride;
-      }
-      return null;  
+  public static UnitSpawnPointOverride GetAnyTaggedLanceMember(this LanceOverride lanceOverride) {
+    List<UnitSpawnPointOverride> unitSpawnOverrides = lanceOverride.unitSpawnPointOverrideList;
+    foreach (UnitSpawnPointOverride unitSpawnOverride in unitSpawnOverrides) {
+      if ((unitSpawnOverride.unitDefId == "Tagged") || (unitSpawnOverride.unitDefId == "UseLance") || (unitSpawnOverride.unitDefId == "mechDef_InheritLance")) return unitSpawnOverride;
+    }
+    return null;
   }
 
   public static bool IsATurretLance(this LanceOverride lanceOverride) {
