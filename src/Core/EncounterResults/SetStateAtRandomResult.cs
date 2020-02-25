d5bd9d33df94e83e2548c776e99175506526e0e7
Richard Griffiths <cwolfs@gmail.com>
2020-02-25 01:04:38 +0100

    Added way to set chunk starting status in contract

--- src/Core/EncounterResults/SetStateAtRandomResult.cs
+++ src/Core/EncounterResults/SetStateAtRandomResult.cs
@@ -9,13 +9,23 @@ namespace MissionControl.Result {
 
     public override void Trigger(MessageCenterMessage inMessage, string triggeringName) {
       Main.LogDebug("[SetStateAtRandomResult] Setting state...");
+      SetStateAtRandom();
+    }
 
+    private void SetStateAtRandom() {
       string encounterGuid = EncounterGuids.GetRandom();
       EncounterObjectGameLogic encounterGameLogic = UnityGameInstance.BattleTechGame.Combat.ItemRegistry.GetItemByGUID<EncounterObjectGameLogic>(encounterGuid);
 
       if (encounterGameLogic != null) {
-        Main.LogDebug($"[SetStateAtRandomResult] Setting '{encounterGameLogic.gameObject.name}' state '{State}'");
-        encounterGameLogic.SetState(State);
+        // A chunk has been disabled by the contract override so ignore it and remove it from the list of choices
+        if ((encounterGameLogic.StartingStatus == EncounterObjectStatus.ControlledByContract) && (encounterGameLogic.GetState() == EncounterObjectStatus.Finished)) {
+          Main.LogDebug($"[SetStateAtRandomResult] Avoiding '{encounterGameLogic.gameObject.name}' due to it not being an active chunk in the contract overrides");
+          EncounterGuids.Remove(encounterGuid);
+          SetStateAtRandom();
+        } else {
+          Main.LogDebug($"[SetStateAtRandomResult] Setting '{encounterGameLogic.gameObject.name}' state '{State}'");
+          encounterGameLogic.SetState(State);
+        }
       } else {
         Main.LogDebug($"[SetStateAtRandomResult] Cannot find EncounterObjectGameLogic with Guid '{encounterGuid}'");
       }
