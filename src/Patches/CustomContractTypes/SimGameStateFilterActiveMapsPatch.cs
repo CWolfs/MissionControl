aaca7efc3a479ed1c203bc170241572712eff546
Richard Griffiths <cwolfs@gmail.com>
2020-05-26 17:18:34 +0200

    Fixed contract spam

--- src/Patches/CustomContractTypes/SimGameStateFilterActiveMapsPatch.cs
+++ src/Patches/CustomContractTypes/SimGameStateFilterActiveMapsPatch.cs
@@ -2,22 +2,45 @@ using Harmony;
 
 using BattleTech;
 using BattleTech.Data;
-using BattleTech.Designed;
 using BattleTech.Framework;
 
-using System;
 using System.Linq;
 using System.Collections.Generic;
 
 namespace MissionControl.Patches {
   [HarmonyPatch(typeof(SimGameState), "FilterActiveMaps")]
   public class SimGameStateFilterActiveMapsPatch {
-    private static bool blockPostfix = false;
-
     static void Postfix(SimGameState __instance, ref WeightedList<MapAndEncounters> activeMaps, List<Contract> currentContracts) {
-      Main.LogDebug($"[SimGameStateFilterActiveMapsPatch.Postfix] Running SimGameStateFilterActiveMapsPatch");
-
+      // Main.LogDebug($"[SimGameStateFilterActiveMapsPatch.Postfix] Running SimGameStateFilterActiveMapsPatch");
+      FixActiveMapWeights(activeMaps);
       FilterOnMapsWithEncountersWithValidContractRequirements(__instance, activeMaps, currentContracts);
+    }
+
+    private static void FixActiveMapWeights(WeightedList<MapAndEncounters> activeMaps) {
+      foreach (MapAndEncounters level in activeMaps) {
+        int indexOfLevelInActiveList = -1;
+        int weight = level.Map.Weight;
+
+        // Get index1 of element in the rootList
+        List<MapAndEncounters> rootList = (List<MapAndEncounters>)Traverse.Create(activeMaps).Field("rootList").GetValue();
+        int indexOfLevelInRootList = rootList.IndexOf(level);
+
+        // Get index2 of element in activeList (if it contains it)
+        List<MapAndEncounters> activeList = (List<MapAndEncounters>)Traverse.Create(activeMaps).Field("activeList").GetValue();
+        if (activeList.Contains(level)) {
+          indexOfLevelInActiveList = activeList.IndexOf(level);
+        }
+
+        // Set the weight in the rootWeights in index1 position
+        List<int> rootWeights = (List<int>)Traverse.Create(activeMaps).Property("rootWeights").GetValue();
+        rootWeights[indexOfLevelInRootList] = weight;
+
+        // if index2 is >= 0, remove from index2 of activeWeights
+        if (indexOfLevelInActiveList >= 0) {
+          List<int> activeWeights = (List<int>)Traverse.Create(activeMaps).Property("activeWeights").GetValue();
+          activeWeights[indexOfLevelInActiveList] = weight;
+        }
+      }
     }
 
     private static void FilterOnMapsWithEncountersWithValidContractRequirements(SimGameState simGameState, WeightedList<MapAndEncounters> activeMaps, List<Contract> currentContracts) {
@@ -36,11 +59,10 @@ namespace MissionControl.Patches {
             // If the contract overrides in the potential contracts by ContractTypeID has a `DoesContractMeetRequirements` sucess, mark remove = false
             List<ContractOverride> contractOverrides = MissionControl.Instance.PotentialContracts[contractTypeId];
             foreach (ContractOverride contractOverride in contractOverrides) {
-
               bool doesContractMeetReqs = (bool)AccessTools.Method(typeof(SimGameState), "DoesContractMeetRequirements").Invoke(simGameState, new object[] { system, level, contractOverride });
               if (doesContractMeetReqs) {
                 // At least one contract override meets the requirements to prevent the infinite spinner so ignore this logic now and continue to the next map/encounter combo
-                Main.LogDebug($"[FilterOnMapsWithEncountersWithValidContractRequirements] Level '{level.Map.MapName}.{encounterLayerMDD.Name}' has at least one valid contract override");
+                // Main.LogDebug($"[FilterOnMapsWithEncountersWithValidContractRequirements] Level '{level.Map.MapName}.{encounterLayerMDD.Name}' has at least one valid contract override");
                 removeMap = false;
                 break;
               }
@@ -49,7 +71,7 @@ namespace MissionControl.Patches {
         }
 
         if (removeMap) {
-          Main.LogDebug($"[FilterOnMapsWithEncountersWithValidContractRequirements] Level '{level.Map.MapName}' had no encounters with anyvalid contract overrides. Removing map.");
+          // Main.LogDebug($"[FilterOnMapsWithEncountersWithValidContractRequirements] Level '{level.Map.MapName}' had no encounters with anyvalid contract overrides. Removing map.");
           indexesToRemove.Add(i);
         }
       }
@@ -61,7 +83,7 @@ namespace MissionControl.Patches {
 
       // If there are no more active maps, reset the biomes/maps list
       if (activeMaps.Count <= 0) {
-        Main.LogDebug($"[FilterOnMapsWithEncountersWithValidContractRequirements] No valid map/encounter combinations. Clearing map discard pile.");
+        // Main.LogDebug($"[FilterOnMapsWithEncountersWithValidContractRequirements] No valid map/encounter combinations. Clearing map discard pile.");
         List<string> mapDiscardPile = (List<string>)AccessTools.Field(typeof(SimGameState), "mapDiscardPile").GetValue(simGameState);
         mapDiscardPile.Clear();
 
