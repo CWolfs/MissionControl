93e11e1874f51428c6e1e743737aa40ad976e134
Richard Griffiths <cwolfs@gmail.com>
2020-06-04 23:38:54 +0200

    Prevent DW running when it shouldn't

--- src/Patches/DynamicWithdraw/CombatHUDRetreatEscMenuOnRetreatConfirmedPatch.cs
+++ src/Patches/DynamicWithdraw/CombatHUDRetreatEscMenuOnRetreatConfirmedPatch.cs
@@ -11,7 +11,7 @@ namespace MissionControl.Patches {
   public class CombatHUDRetreatEscMenuOnRetreatConfirmedPatch {
     [HarmonyBefore(new string[] { "us.frostraptor.DisorderlyWithdrawal" })]
     static bool Prefix(CombatHUDRetreatEscMenu __instance) {
-      if (Main.Settings.DynamicWithdraw.Enable) {
+      if (MissionControl.Instance.AllowMissionControl() && Main.Settings.DynamicWithdraw.Enable) {
         Main.LogDebug("[CombatHUDRetreatEscMenuOnRetreatConfirmedPatch] Sending Dynamic Withdraw Trigger Message");
         ChunkMessage triggerDynamicWithdrawMessage = new ChunkMessage(ChunkLogic.DYNAMIC_WITHDRAW_CHUNK_GUID);
         UnityGameInstance.Instance.Game.Combat.MessageCenter.PublishMessage(triggerDynamicWithdrawMessage);
