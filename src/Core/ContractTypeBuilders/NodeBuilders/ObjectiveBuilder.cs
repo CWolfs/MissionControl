2275a5ea324c6b468cae6335faa1aa5dee54544f
Richard Griffiths <cwolfs@gmail.com>
2020-06-07 01:45:11 +0200

    Set a default of `Rounds` for durationType on the DefendXUnits objective

--- src/Core/ContractTypeBuilders/NodeBuilders/ObjectiveBuilder.cs
+++ src/Core/ContractTypeBuilders/NodeBuilders/ObjectiveBuilder.cs
@@ -138,7 +138,7 @@ namespace MissionControl.ContractTypeBuilders {
       string[] requiredTagsOnUnit = (objective.ContainsKey("RequiredTagsOnUnit")) ? ((JArray)objective["RequiredTagsOnUnit"]).ToObject<string[]>() : null;
       int numberOfUnitsToDefend = (objective.ContainsKey("NumberOfUnitsToDefend")) ? ((int)objective["NumberOfUnitsToDefend"]) : 1;
       int durationToDefend = (objective.ContainsKey("DurationToDefend")) ? (int)objective["DurationToDefend"] : 0;
-      string durationTypeStr = (objective.ContainsKey("DurationType")) ? objective["DurationType"].ToString() : null;
+      string durationTypeStr = (objective.ContainsKey("DurationType")) ? objective["DurationType"].ToString() : "Rounds";
       string progressFormat = (objective.ContainsKey("ProgressFormat")) ? objective["ProgressFormat"].ToString() : "";
       string description = objective["Description"].ToString();
 
