053e47b3953ddb38dd0fab706ddd51c1492253c3
Richard Griffiths <cwolfs@gmail.com>
2021-08-05 12:23:14 +0200

    Add a max influence used to clamp the drop weight influence

--- src/Core/Settings/AdditionalLances/DropWeightInfluenceSettings.cs
+++ src/Core/Settings/AdditionalLances/DropWeightInfluenceSettings.cs
@@ -7,6 +7,12 @@ namespace MissionControl.Config {
     [JsonProperty("Enable")]
     public bool Enable { get; set; } = false;
 
+    [JsonProperty("EnemySpawnInfluenceMax")]
+    public float EnemySpawnInfluenceMax { get; set; } = 0.9f;
+
+    [JsonProperty("AllySpawnInfluenceMax")]
+    public float AllySpawnInfluenceMax { get; set; } = 0.9f;
+
     [JsonProperty("EnemySpawnInfluencePerHalfSkullAbove")]
     public float EnemySpawnInfluencePerHalfSkullAbove { get; set; } = 0.1f;
 
@@ -18,5 +24,16 @@ namespace MissionControl.Config {
 
     [JsonProperty("AllySpawnInfluencePerHalfSkullBelow")]
     public float AllySpawnInfluencePerHalfSkullBelow { get; set; } = -0.1f;
+
+    public float GetSpawnInfluenceMax(string team) {
+      if (team == "Enemy") {
+        return EnemySpawnInfluenceMax;
+      } else if (team == "Allies") {
+        return AllySpawnInfluenceMax;
+      }
+
+      Main.Logger.LogError("[GetSpawnInfluenceMax] Team '{team}' is invalid. Must be 'Enemy' or 'Allies'. Returning 0.9");
+      return 0.9f;
+    }
   }
 }
