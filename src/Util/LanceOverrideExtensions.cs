0eaaa90a53fda8c8482891e183f687225d03c459
Richard Griffiths <cwolfs@gmail.com>
2022-01-17 17:34:11 +0100

    Added turretDef_InheritLance to a utility check

--- src/Util/LanceOverrideExtensions.cs
+++ src/Util/LanceOverrideExtensions.cs
@@ -14,7 +14,7 @@ public static class LanceOverrideExtensions {
   public static UnitSpawnPointOverride GetAnyTaggedLanceMember(this LanceOverride lanceOverride) {
     List<UnitSpawnPointOverride> unitSpawnOverrides = lanceOverride.unitSpawnPointOverrideList;
     foreach (UnitSpawnPointOverride unitSpawnOverride in unitSpawnOverrides) {
-      if ((unitSpawnOverride.unitDefId == "Tagged") || (unitSpawnOverride.unitDefId == "UseLance") || (unitSpawnOverride.unitDefId == "mechDef_InheritLance") || (unitSpawnOverride.unitDefId == "vehicleDef_InheritLance")) return unitSpawnOverride;
+      if ((unitSpawnOverride.unitDefId == "Tagged") || (unitSpawnOverride.unitDefId == "UseLance") || (unitSpawnOverride.unitDefId == "mechDef_InheritLance") || (unitSpawnOverride.unitDefId == "vehicleDef_InheritLance") || (unitSpawnOverride.unitDefId == "turretDef_InheritLance")) return unitSpawnOverride;
     }
     return null;
   }
