1db24c6184e780f138a013fb6245a30995447ba5
Richard Griffiths <cwolfs@gmail.com>
2022-04-13 22:14:39 +0200

    Fixed elite lances always being selected for

--- src/Core/Settings/EliteLances.cs
+++ src/Core/Settings/EliteLances.cs
@@ -18,7 +18,7 @@ namespace MissionControl.Config {
     public bool ShouldEliteLancesBeSelected(FactionDef faction) {
       if (!Main.Settings.AdditionalLanceSettings.UseElites) return false;
       if (MissionControl.Instance.IsSkirmish()) return false;
-      bool useEliteLances = true;
+      bool useEliteLances = false;
 
       if (useEliteLances && Conditions.Contains("IsEnemy")) {
         useEliteLances = UnityGameInstance.Instance.Game.Simulation.IsFactionEnemy(faction.FactionValue);
