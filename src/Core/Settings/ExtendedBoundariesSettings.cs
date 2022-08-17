0bb9122d96d92673220f2d6a4bed90cc63fc211e
Richard Griffiths <cwolfs@gmail.com>
2022-08-17 10:03:31 +0200

    Added EB feature - needs testing though

--- src/Core/Settings/ExtendedBoundariesSettings.cs
+++ src/Core/Settings/ExtendedBoundariesSettings.cs
@@ -11,11 +11,20 @@ namespace MissionControl.Config {
     [JsonProperty("Overrides")]
     public List<ExtendedBoundariesOverride> Overrides { get; set; } = new List<ExtendedBoundariesOverride>();
 
+    private bool hasSortedOverrides = false;
+
     public float GetSizePercentage(string mapId, string contractTypeName) {
+      if (!hasSortedOverrides) {
+        Overrides = Overrides.OrderBy(x => x.MapId != "UNSET" && x.ContractTypeName != "UNSET").ToList();
+        hasSortedOverrides = true;
+      }
+
       string id = $"{mapId}.{contractTypeName}";
 
       foreach (ExtendedBoundariesOverride ovr in Overrides) {
-        if (ovr.MapId == "UNSET") ovr.MapId = mapId;  // Allow for fuzzy map matching
+        // Allow for fuzzy map matching
+        if (ovr.MapId == "UNSET") ovr.MapId = mapId;
+        if (ovr.ContractTypeName == "UNSET") ovr.ContractTypeName = contractTypeName;
 
         if (ovr.Id == id) return ovr.IncreaseBoundarySizeByPercentage;
       }
