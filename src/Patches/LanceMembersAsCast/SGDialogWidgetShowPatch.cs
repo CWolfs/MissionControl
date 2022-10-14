c9b0d6f30968b6340fa72621ed48de11fb7b0054
Richard Griffiths <cwolfs@gmail.com>
2022-10-14 22:44:56 +0200

    Fixed dropship conversation crashing

--- src/Patches/LanceMembersAsCast/SGDialogWidgetShowPatch.cs
+++ src/Patches/LanceMembersAsCast/SGDialogWidgetShowPatch.cs
@@ -11,11 +11,12 @@ namespace MissionControl.Patches {
   [HarmonyPatch(typeof(SGDialogWidget), "Show")]
   public class SGDialogWidgetShowPatch {
     public static void Prefix(SGDialogWidget __instance, ref string text, ref CastDef whoIsTalking) {
-      DialogueInterpolator.Instance.HandleDeadActorFromDialogueContent(ref whoIsTalking);
+      if (UnityGameInstance.Instance.Game.Combat != null) {
+        DialogueInterpolator.Instance.HandleDeadActorFromDialogueContent(ref whoIsTalking);
+      }
 
       if (text == null) return;
       text = DialogueInterpolator.Instance.Interpolate(DialogueInterpolator.InterpolateType.PreInterpolate, text, whoIsTalking);
-
     }
 
     public static void Postfix(SGDialogWidget __instance, ConvDialogEntry ___currentEntry) {
