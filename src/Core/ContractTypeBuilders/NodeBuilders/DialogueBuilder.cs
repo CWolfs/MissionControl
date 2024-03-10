ad7dca7eb24dc9397f372707cd8290f0b04f02a9
Richard Griffiths <cwolfs@gmail.com>
2024-03-10 22:13:04 +0100

    Added support for Dialogue Sequence Node

--- src/Core/ContractTypeBuilders/NodeBuilders/DialogueBuilder.cs
+++ src/Core/ContractTypeBuilders/NodeBuilders/DialogueBuilder.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
 using System;
+using System.Collections.Generic;
 
 using MissionControl.Trigger;
-using MissionControl.Rules;
 using MissionControl.EncounterFactories;
 using MissionControl.Messages;
 
@@ -12,42 +12,42 @@ using Newtonsoft.Json.Linq;
 namespace MissionControl.ContractTypeBuilders {
   public class DialogueBuilder : NodeBuilder {
     private ContractTypeBuilder contractTypeBuilder;
-    private JObject objective;
+    private JObject dialogue;
 
     private GameObject parent;
     private string name;
     private string subType;
     private string guid;
-    private string trigger;
-    private bool showOnlyOnce;
 
-    public DialogueBuilder(ContractTypeBuilder contractTypeBuilder, GameObject parent, JObject objective) {
+    public DialogueBuilder(ContractTypeBuilder contractTypeBuilder, GameObject parent, JObject dialogue) {
       this.contractTypeBuilder = contractTypeBuilder;
-      this.objective = objective;
+      this.dialogue = dialogue;
 
       this.parent = parent;
-      this.name = objective["Name"].ToString();
-      this.subType = objective["SubType"].ToString();
-      this.guid = objective["Guid"].ToString();
-      this.trigger = objective.ContainsKey("Trigger") ? objective["Trigger"].ToString() : null;
-      this.showOnlyOnce = objective.ContainsKey("ShowOnlyOnce") ? (bool)objective["ShowOnlyOnce"] : false;
+      this.name = dialogue["Name"].ToString();
+      this.subType = dialogue["SubType"].ToString();
+      this.guid = dialogue["Guid"].ToString();
     }
 
     public override void Build() {
       switch (subType) {
-        case "Simple": BuildSimpleDialogue(); break;
+        case "Simple": BuildSimpleDialogue(parent, dialogue); break;
+        case "Sequence": BuildSequenceDialogue(parent, dialogue); break;
         default: Main.LogDebug($"[DialogueBuilder.{contractTypeBuilder.ContractTypeKey}] No support for sub-type '{subType}'. Check for spelling mistakes."); break;
       }
     }
 
-    private void BuildSimpleDialogue() {
-      DialogueFactory.CreateDialogLogic(this.parent, this.name, this.guid, this.showOnlyOnce);
+    private void BuildSimpleDialogue(GameObject parent, JObject dialogue) {
+      string trigger = dialogue.ContainsKey("Trigger") ? dialogue["Trigger"].ToString() : null;
+      bool showOnlyOnce = dialogue.ContainsKey("ShowOnlyOnce") ? (bool)dialogue["ShowOnlyOnce"] : false;
+
+      DialogueFactory.CreateDialogLogic(parent, this.name, this.guid, showOnlyOnce);
 
       MessageCenterMessageType messageType;
-      if (this.trigger != null) {
-        if (!Enum.TryParse(this.trigger, out messageType)) {
+      if (trigger != null) {
+        if (!Enum.TryParse(trigger, out messageType)) {
           MessageTypes customMessageType;
-          if (!Enum.TryParse(this.trigger, out customMessageType)) {
+          if (!Enum.TryParse(trigger, out customMessageType)) {
             Main.Logger.LogError("[DialogueBuilder] Invalid 'Trigger' provided.");
           } else {
             messageType = (MessageCenterMessageType)customMessageType;
@@ -57,6 +57,13 @@ namespace MissionControl.ContractTypeBuilders {
         DialogTrigger dialogueTrigger = new DialogTrigger(messageType, this.guid);
         dialogueTrigger.Run();
       }
+    }
+
+    private void BuildSequenceDialogue(GameObject parent, JObject dialogue) {
+      bool isInterrupt = dialogue.ContainsKey("IsInterrupt") ? (bool)dialogue["IsInterrupt"] : true;
+      List<string> dialogueGUIDs = dialogue.ContainsKey("DialogueGUIDs") ? dialogue["DialogueGUIDs"].ToObject<List<string>>() : null;
+
+      DialogueFactory.CreateDialogueSequenceLogic(parent, this.name, this.guid, isInterrupt, dialogueGUIDs);
     }
   }
 }
