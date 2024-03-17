7ca45cb474d936d4ca1cafcf810e778d3b486258
Richard Griffiths <cwolfs@gmail.com>
2024-03-17 10:37:51 +0100

    Added warnings on SetStatusResult if Objective is Ignored

--- src/Core/EncounterResults/State/SetStatusResult.cs
+++ src/Core/EncounterResults/State/SetStatusResult.cs
@@ -1,4 +1,5 @@
 using BattleTech;
+using BattleTech.Framework;
 
 namespace MissionControl.Result {
   public class SetStatusResult : EncounterResult {
@@ -15,6 +16,12 @@ namespace MissionControl.Result {
           Main.LogDebug($"[SetStatusResult] Avoiding '{encounterGameLogic.gameObject.name}' due to it not being an active chunk in the contract overrides");
         } else {
           Main.LogDebug($"[SetStatusResult] Setting '{encounterGameLogic.gameObject.name}' state '{Status}'");
+
+          if (encounterGameLogic is ObjectiveGameLogic objectiveGameLogic && objectiveGameLogic.currentObjectiveStatus != ObjectiveStatus.Active) {
+            Main.Logger.LogWarning($"[SetStatusResult] Setting the Objective '{encounterGameLogic.gameObject.name}' Encounter Object state to '{Status}' but the Objective status is not 'Active' (these are different statuses). Current Objective status is '{objectiveGameLogic.currentObjectiveStatus}'. This will mean the Objective will not run.");
+            if (objectiveGameLogic.currentObjectiveStatus == ObjectiveStatus.Ignored) Main.Logger.LogWarning($"Do not set Chunks to 'Ignored' if you want to use them again. This sets all Objectives to 'Ignored'.");
+          }
+
           encounterGameLogic.SetState(Status);
         }
       } else {
