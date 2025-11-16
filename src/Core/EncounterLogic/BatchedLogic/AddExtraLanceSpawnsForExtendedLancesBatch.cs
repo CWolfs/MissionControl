3caf549def0d6dfbc91bcaf580452fbcb8d501e5
Richard Griffiths <cwolfs@gmail.com>
2025-11-16 11:04:34 +0100

    Remove unused imports

--- src/Core/EncounterLogic/BatchedLogic/AddExtraLanceSpawnsForExtendedLancesBatch.cs
+++ src/Core/EncounterLogic/BatchedLogic/AddExtraLanceSpawnsForExtendedLancesBatch.cs
@@ -1,9 +1,4 @@
-using UnityEngine;
-using System;
-using System.Collections.Generic;
-
 using MissionControl.Rules;
-using MissionControl.Trigger;
 
 namespace MissionControl.Logic {
   public class AddExtraLanceSpawnsForExtendedLancesBatch {
